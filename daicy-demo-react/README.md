68af0fdc69381c8b363084128692a39cfb4d3875
cqz <qzcheng@daicy.net>
2020-07-22 19:00:53 +0800

    init

--- daicy-demo-react/README.md
+++ daicy-demo-react/README.md
@@ -2,7 +2,7 @@
 ## 欢迎参加Daicy 机试
  * 请在项目中完成一下需求：
      *  使用`antd`，Table、Input 等组件完成一个表格，渲染`store`中（自己创建对应store）数据，并完成增删改查功能，
-                 需要经过一下阶段 `store 创建->定义初始数据->定义action->触发对应需求action->更新store数据-> view显示更新后数据`即可，相关的技术可以进行百度等；
+                 需要经过以下阶段  `store 创建->定义初始数据->定义action->触发对应需求action->更新store数据-> view显示更新后数据`即可，相关的技术可以进行百度等；
      * main 下创建自己的目录为demo
         * 新增加路由  `demo` component 为demo 下对应的组件；
         * 请区分组件的state 和 全局的store
--- daicy-demo-react/src/main/welcome/index.js
+++ daicy-demo-react/src/main/welcome/index.js
@@ -8,7 +8,7 @@ export default  class Welcome extends Component {
         let md = "## 欢迎参加Daicy 机试\n" +
             " * 请在项目中完成一下需求：\n" +
             "     *  使用`antd`，Table、Input 等组件完成一个表格，渲染`store`中（自己创建对应store）数据，并完成增删改查功能，\n" +
-            "                 需要经过一下阶段 `store 创建->定义初始数据->定义action->触发对应需求action->更新store数据-> view显示更新后数据`即可，相关的技术可以进行百度等；\n" +
+            "                 需要经过以下阶段  `store 创建->定义初始数据->定义action->触发对应需求action->更新store数据-> view显示更新后数据`即可，相关的技术可以进行百度等；\n" +
             "     * main 下创建自己的目录为demo\n" +
             "        * 新增加路由  `demo` component 为demo 下对应的组件；\n" +
             "        * 请区分组件的state 和 全局的store\n" +
